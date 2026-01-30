b8045fd6b4446185d25bc1292254f10073480d71
César García <cesar.garciamcs@proton.me>
2026-01-30 14:13:18 +0000

    iniciando configuración de express con rutas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
-import { iniciar } from "./server"
+import server from "./server"
 
-console.log("Iniciando servidor...")
-
-iniciar()
+server.listen(4000, () => {
+    console.log(`REST API en el puerto 4000`)
+})
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,26 @@
-export function iniciar() {
-    console.log("Ejecutando servidor...")
-}
+import express from "express"
+
+const server  = express()
+
+// Routing
+server.get('/', (req, res) => {
+    res.send("Desde get")
+})
+
+server.post('/', (req, res) => {
+    res.send("Desde post")
+})
+
+server.put('/', (req, res) => {
+    res.send("Desde put")
+})
+
+server.patch('/', (req, res) => {
+    res.send("Desde patch")
+})
+
+server.delete('/', (req, res) => {
+    res.send("Desde delete")
+})
+
+export default server
